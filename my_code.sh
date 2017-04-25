c81d9b694124035ed8ac7fece5d4bfa8e968689c
Sala K405 <salak405@itmsalas.edu.co>
2017-04-24 21:29:35 -0500

    create table EMPLOYEES (
      Employee_ID    number(6) not null,
      First_Name    varchar2(20) ,
      Last_Name    varchar2(25)  not null,
      Email     varchar2(25)  not null,
      Phone_number varchar2(20)  ,
      Hire_Date    date      not null,
      Job_ID    varchar2(10)  not null,
      salary number(8,2),
      Commision_PCT number(2,2),
      Managemer_ID   number(6),
      Departament_ID   number(4)
    );
    
    constraint EMPLOYEES_salary primary key (salary)
    create table EMPLOYEES (
    
      Employee_ID    number(6) not null,
      First_Name    varchar2(20) ,
      Last_Name    varchar2(25)  not null,
      Email     varchar2(25)  not null,
      Phone_number varchar2(20)  ,
      Hire_Date    date      not null,
      Job_ID    varchar2(10)  not null,
      salary number(8,2),
      Commision_PCT number(2,2),
      Managemer_ID   number(6),
      Departament_ID   number(4)
    );
    
    constraint EMPLOYEES_salary primary key (salary)
    create table EMPLOYEES (
    
      Employee_ID    number(6) not null,
      First_Name    varchar2(20) ,
      Last_Name    varchar2(25)  not null,
      Email     varchar2(25)  not null,
      Phone_number varchar2(20)  ,
      Hire_Date    date      not null,
      Job_ID    varchar2(10)  not null,
      salary number(8,2),
      Commision_PCT number(2,2),
      Managemer_ID   number(6),
      Departament_ID   number(4)
    );
    
    constraint EMPLOYEES_salary primary key (salary)
    create table EMPLOYEES (
    
      Employee_ID    number(6) not null,
      First_Name    varchar2(20) ,
      Last_Name    varchar2(25)  not null,
      Email     varchar2(25)  not null,
      Phone_number varchar2(20)  ,
      Hire_Date    date      not null,
      Job_ID    varchar2(10)  not null,
      salary number(8,2),
      Commision_PCT number(2,2),
      Managemer_ID   number(6),
      Departament_ID   number(4)
    );
    
    constraint EMPLOYEES_salary primary key (salary)
    create table EMPLOYEES (
    
      Employee_ID    number(6) not null,
      First_Name    varchar2(20) ,
      Last_Name    varchar2(25)  not null,
      Email     varchar2(25)  not null,
      Phone_number varchar2(20)  ,
      Hire_Date    date      not null,
      Job_ID    varchar2(10)  not null,
      salary number(8,2),
      Commision_PCT number(2,2),
      Managemer_ID   number(6),
      Departament_ID   number(4)
    );
    
    constraint EMPLOYEES_salary primary key (salary)
    create table EMPLOYEES (
    
      Employee_ID    number(6) not null,
      First_Name    varchar2(20) ,
      Last_Name    varchar2(25)  not null,
      Email     varchar2(25)  not null,
      Phone_number varchar2(20)  ,
      Hire_Date    date      not null,
      Job_ID    varchar2(10)  not null,
      salary number(8,2),
      Commision_PCT number(2,2),
      Managemer_ID   number(6),
      Departament_ID   number(4)
    );
    
    constraint EMPLOYEES_salary primary key (salary)

--- my_code.sh
+++ my_code.sh
@@ -1,1 +1,21 @@
 echo "Hello World"
+
+
+
+
+create table EMPLOYEES (
+
+  Employee_ID    number(6) not null,
+  First_Name    varchar2(20) ,
+  Last_Name    varchar2(25)  not null,
+  Email     varchar2(25)  not null,
+  Phone_number varchar2(20)  ,
+  Hire_Date    date      not null,
+  Job_ID    varchar2(10)  not null,
+  salary number(8,2),
+  Commision_PCT number(2,2),
+  Managemer_ID   number(6),
+  Departament_ID   number(4)
+);
+
+constraint EMPLOYEES_salary primary key (salary)
